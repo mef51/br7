f6de22bad69f0410b9d6789ada976d7d21e06561
mohammed chamma <hms.mohammed@gmail.com>
2013-09-09 20:40:17 -0400

    Add a dev helper to reload the page when a page is focused

--- js/br7.js
+++ js/br7.js
@@ -1,4 +1,9 @@
 
+// this is for developing. Will refresh the page whenever it is focused
+window.onfocus = function() {
+    location.reload();
+}
+
 // helper for including html files in a page
 include = function(id) {
     $("#" + id).load(id + ".html");
